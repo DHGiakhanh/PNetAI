23431356fe3f761a9b8682920bb34f56c301050e
RianPPP <phuongptmqe180193@fpt.edu.vn>
2026-05-29 10:55:35 +0700

    Revert "update Request qua Cloudflare"
    This reverts commit 90694e032b4fb6ba2c7789650fa4ad2322e7c533.

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath } from 'url'
 import path from 'path'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   plugins: [react()],
@@ -16,12 +12,6 @@ export default defineConfig({
   server: {
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
-    },
-    // Cho phép link Cloudflare truy cập
-    allowedHosts: true,
-    // Cấu hình định tuyến WebSocket để không bị lỗi 400 Bad Request qua Cloudflare
-    hmr: {
-      clientPort: 443,
     },
   },
 })
